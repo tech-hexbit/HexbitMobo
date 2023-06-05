bbd30169020c64a32844e3b6f6541c1429d5e618
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 21:30:04 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,7 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
+// theme
 import { COLORS } from "../constants/theme";
+
+// img
+import img1 from "./../../assets/Login/logo.png";
+import img2 from "./../../assets/Login/Cash Payment-bro 1.png";
 
 const Login = () => {
   return (
