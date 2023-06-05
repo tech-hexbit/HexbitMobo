f34503e8dea6a02fbc22210d719e0ce5a310eb2c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 05:51:52 +0530

    assets

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import { View, Text } from "react-native";
 import { Link, Stack } from "expo-router";
 
+// components
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <View>
       {/* Use the `Screen` component to configure the layout. */}
       <Stack.Screen options={{ title: "Home" }} />
       {/* Use the `Link` component to enable optimized client-side routing. */}
+      <Header pageName="pageName" />
       <Text>Hello World</Text>
     </View>
   );
