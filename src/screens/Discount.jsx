34d1f82d3c956d7861952d18402c0efc37b266da
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 19:26:29 +0530

    Create Discount.jsx

---
+++ src/screens/Discount.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Discount = () => {
+  return (
+    <View>
+      <Text>Discount</Text>
+    </View>
+  );
+};
+
+export default Discount;
