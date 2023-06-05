2b2328cf94c5103f2ab9a68a34eb3c5e8511c625
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 19:18:33 +0530

    Create theme.js

---
+++ src/constants/theme.js
@@ -0,0 +1,59 @@
+const COLORS = {
+  primary: "#350b5e",
+  secondary: "#444262",
+  tertiary: "#FF7754",
+
+  bgScreen: "#F3EFFF",
+
+  ActivityBlock: "#7d629b",
+
+  Amt: "#595959",
+  OrNo: "#24385e",
+  Shipped: "#216445",
+
+  gray: "#83829A",
+  gray2: "#C1C0C8",
+
+  white: "#F3F4F8",
+  lightWhite: "#FAFAFC",
+};
+
+const FONT = {
+  regular: "DMRegular",
+  medium: "DMMedium",
+  bold: "DMBold",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
