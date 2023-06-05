b6e07ae51503363119637aa046255e0e351d751e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 19:30:44 +0530

    Update Inventory.jsx

--- src/screens/Inventory.jsx
+++ src/screens/Inventory.jsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
+
+// components
+import Header from "./../Components/Header";
+import Sales from "./../Components/Order/Sales";
+import InventoryC from "./../Components/Order/Inventory";
+
+// theme
+import { COLORS } from "../constants/theme";
 
 const Inventory = () => {
   return (
-    <View>
-      <Text>Inventory</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={sty.mDiv}>
+        <Header pageName="Orders" />
+
+        <Sales />
+        <InventoryC />
+      </View>
+    </ScrollView>
   );
 };
+
+const sty = StyleSheet.create({
+  mDiv: {
+    padding: 20,
+    backgroundColor: COLORS.bgScreen,
+  },
+});
 
 export default Inventory;
