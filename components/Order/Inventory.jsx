78d39f50df49d3810040ec89f5580adae7693bca
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 06:59:05 +0530

    Update Inventory.jsx

--- components/Order/Inventory.jsx
+++ components/Order/Inventory.jsx
@@ -9,7 +9,14 @@ const Inventory = () => {
     <View>
       <Text style={InventoryCss.StatusText}>Inventory Status</Text>
       <View style={InventoryCss.tabView}>
-        <Text>S</Text>
+        <View style={InventoryCss.ViewRow}>
+          <Text style={InventoryCss.txt1}>QUANTITY IN HAND</Text>
+          <Text style={InventoryCss.txt2}>5768</Text>
+        </View>
+        <View style={InventoryCss.ViewRow}>
+          <Text style={InventoryCss.txt3}>QUANTITY TO BE RECEIVED</Text>
+          <Text style={InventoryCss.txt4}>674</Text>
+        </View>
       </View>
     </View>
   );
@@ -23,7 +30,44 @@ const InventoryCss = StyleSheet.create({
     marginBottom: 15,
   },
   tabView: {
-    backgroundColor: COLORS.primary,
+    backgroundColor: COLORS.ActivityBlock,
+    borderRadius: 10,
+  },
+  ViewRow: {
+    flexDirection: "row",
+    // justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+  },
+  txt1: {
+    color: "white",
+    padding: 7,
+    fontWeight: 500,
+    fontSize: 16,
+    width: 176,
+    // backgroundColor: "red",
+    borderColor: "white",
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+  },
+  txt2: {
+    color: "white",
+    padding: 7,
+    fontWeight: 500,
+    fontSize: 16,
+    width: 176,
+    // backgroundColor: "red",
+    borderColor: "white",
+    borderBottomWidth: 1,
+
+    // width: 50,
+    // backgroundColor: "red",
+  },
+  txt3: {
+    color: "white",
+  },
+  txt4: {
+    color: "white",
   },
 });
 
