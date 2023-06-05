c7771a03ae59349f38fb4c3226920159896e2e4f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 21:29:01 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,14 +1,22 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
+import { COLORS } from "../constants/theme";
+
 const Login = () => {
   return (
-    <View>
+    <View style={LoginCss.mDIv}>
       <Text>Login</Text>
     </View>
   );
 };
 
-const LoginCss = StyleSheet.create({});
+const LoginCss = StyleSheet.create({
+  mDIv: {
+    backgroundColor: COLORS.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Login;
