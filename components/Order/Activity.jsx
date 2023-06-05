de04fc405cb9bd0fbbe2c30d960dd88b51c7f425
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 06:32:37 +0530

    Update Activity.jsx

--- components/Order/Activity.jsx
+++ components/Order/Activity.jsx
@@ -17,16 +17,22 @@ const ActivityCss = StyleSheet.create({
   ActivityBlock: {
     backgroundColor: COLORS.ActivityBlock,
     borderRadius: 10,
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 25,
+    paddingRight: 25,
   },
   amtText: {
     color: "white",
     fontWeight: 500,
     fontSize: 30,
+    textAlign: "center",
   },
   statusText: {
     color: "white",
     fontWeight: 500,
     fontSize: 20,
+    textAlign: "center",
   },
 });
 
