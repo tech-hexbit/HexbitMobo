869d3d0afb30b1dedb30fdfe2ffab2a622da61b5
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 05:47:36 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
-import { View, Text } from "react-native-web";
+import { View, Text } from "react-native";
+import { Link, Stack } from "expo-router";
 
-const Home = () => {
+export default function Home() {
   return (
     <View>
+      {/* Use the `Screen` component to configure the layout. */}
+      <Stack.Screen options={{ title: "Home" }} />
+      {/* Use the `Link` component to enable optimized client-side routing. */}
       <Text>Hello World</Text>
     </View>
   );
-};
-
-export default Layout;
+}
