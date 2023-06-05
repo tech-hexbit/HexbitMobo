f1e64c1ae19b7408ba9b216c348478ae3cbfe05f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 06:04:54 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import { Link, Stack } from "expo-router";
 
 // components
 import Header from "../components/Header";
+import Sales from "../components/Order/Sales";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Stack.Screen options={{ title: "Home" }} />
       {/* Use the `Link` component to enable optimized client-side routing. */}
       <Header pageName="Orders" />
+      <Sales />
       <Text>Hello World</Text>
     </View>
   );
