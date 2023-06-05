7550468b9172cdda595d117ba4d4f7c6e2b3feda
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 06:07:45 +0530

    Update Sales.jsx

--- components/Order/Sales.jsx
+++ components/Order/Sales.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+import { COLORS, SIZES } from "../../constants/theme";
 
 const Sales = () => {
   return (
-    <View>
+    <View style={SalesCss.mDiv}>
       <Text>Sales</Text>
     </View>
   );
 };
+
+const SalesCss = StyleSheet.create({
+  mDiv: {
+    margin: 20,
+  },
+});
 
 export default Sales;
