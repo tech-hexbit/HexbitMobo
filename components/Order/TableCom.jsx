bb347f2bd3c6e8f30424f813657e691d8ffd67df
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 08:42:25 +0530

    Update TableCom.jsx

--- components/Order/TableCom.jsx
+++ components/Order/TableCom.jsx
@@ -21,10 +21,10 @@ const TableCom = () => {
   ]);
 
   const [tableData, settableData] = useState([
-    ["04 Aug 2023", "04 Aug 2023", "04 Aug 2023", "04 Aug 2023"],
-    ["#6745867", "#6745867", "#6745867", "#6745867"],
-    ["Shipped", "Shipped", "Shipped", "Shipped"],
-    ["645", "564", "456", "564"],
+    ["04 Aug 2023", "#6745867", "Shipped", "423"],
+    ["04 Aug 2023", "#6745867", "Shipped", "674"],
+    ["04 Aug 2023", "#6745867", "Shipped", "465"],
+    ["04 Aug 2023", "#6745867", "Shipped", "564"],
   ]);
 
   return (
@@ -42,15 +42,16 @@ const TableCom = () => {
                 <Cell
                   key={k}
                   data={cellData}
+                  style={TableCss.CellCss}
                   textStyle={
                     k === 2
-                      ? { color: COLORS.Shipped }
+                      ? { color: COLORS.Shipped, textAlign: "center" }
                       : k === 3
-                      ? { color: COLORS.Amt }
+                      ? { color: COLORS.Amt, textAlign: "center" }
                       : k == 1
-                      ? { color: COLORS.OrNo }
+                      ? { color: COLORS.OrNo, textAlign: "center" }
                       : k === 0
-                      ? { color: COLORS.Amt }
+                      ? { color: COLORS.Amt, textAlign: "center" }
                       : ""
                   }
                 />
@@ -64,46 +65,45 @@ const TableCom = () => {
 };
 
 const TableCss = StyleSheet.create({
-  // container: {
-  //   // padding: 15,
-  //   backgroundColor: "white",
-  //   borderRadius: 10,
-  // },
+  container: {
+    flex: 1,
+    padding: 10,
+    // paddingTop: 30,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  head: {
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "black",
+    // height: 40,
+    // backgroundColor: "#808B97",
+  },
   // head: {
-  //   padding: 5,
   //   borderColor: "black",
-  //   borderBottomWidth: 1,
   // },
   // head2: {
   //   padding: 20,
-  //   borderColor: "black",
-  //   borderBottomWidth: 1,
   // },
-  // text1: {
-  //   textAlign: "center",
-  //   color: COLORS.primary,
-  //   fontWeight: 500,
-  //   fontSize: 14,
-  // },
+  text1: {
+    textAlign: "center",
+    color: COLORS.primary,
+    fontWeight: 500,
+    fontSize: 13,
+  },
   // text2: {
   //   textAlign: "center",
   // },
-  container: {
-    flex: 1,
-    padding: 15,
-    paddingTop: 30,
-    backgroundColor: "#fff",
-  },
-  head: {
-    height: 40,
-    backgroundColor: "#808B97",
-  },
-  text: {
-    margin: 6,
-  },
+
   row: {
     flexDirection: "row",
-    backgroundColor: "#FFF1C1",
+    borderBottomWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "black",
+    padding: 10,
+
+    // backgroundColor: "#FFF1C1",
   },
 });
 
