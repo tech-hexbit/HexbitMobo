15d72a1b43de4796ee010c92c4c33b25a6d60b85
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 06:36:27 +0530

    Update Inventory.jsx

---
+++ components/Order/Inventory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Inventory = () => {
+  return (
+    <View>
+      <Text>Inventory Status</Text>
+    </View>
+  );
+};
+
+export default Inventory;
