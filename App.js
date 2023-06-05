3744b71a128f70e844c4434b48270a75e129d620
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-05 17:53:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // screens
 import Home from "./src/screens/Home";
 import Inventory from "./src/screens/Inventory";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Home />
-      <Inventory />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Inventory" component={Inventory} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
