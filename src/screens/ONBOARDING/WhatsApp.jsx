c40768efd91d144f775d0701c0865d7f2ecb7f88
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 08:51:44 +0530

    Create WhatsApp.jsx

---
+++ src/screens/ONBOARDING/WhatsApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// Components
+import Header from "../../Components/ONBOARDING/Header";
+
+const WhatsApp = () => {
+  return (
+    <View>
+      <Text>WhatsApp</Text>
+    </View>
+  );
+};
+
+export default WhatsApp;
