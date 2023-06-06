89dfff3a8727a1d5698872914d25b4da8a3b5e9c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 16:53:32 +0530

    Create InpOne.jsx

---
+++ src/Components/Cart/InpOne.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const InpOne = () => {
+  return (
+    <View>
+      <Text>InpOne</Text>
+    </View>
+  );
+};
+
+export default InpOne;
