b6cfa25ef72c19c3f05bf413a46b779bb85c7282
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 23:50:22 +0530

    Create StoreHeader.jsx

---
+++ src/Components/Cart/StoreHeader.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const StoreHeader = () => {
+  return (
+    <View>
+      <Text>StoreHeader</Text>
+    </View>
+  );
+};
+
+export default StoreHeader;
