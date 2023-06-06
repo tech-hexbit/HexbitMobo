482377b7470daf798bdfc55c10464f42fa86ca4b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 13:15:00 +0530

    Create Email.jsx

---
+++ src/screens/ONBOARDING/Email.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Email = () => {
+  return (
+    <View>
+      <Text>Email</Text>
+    </View>
+  );
+};
+
+export default Email;
