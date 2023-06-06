503f60124219c78ae3f2e4b9f151b922ad5b0f7f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 10:49:33 +0530

    Update Otp.jsx

--- src/screens/ONBOARDING/Otp.jsx
+++ src/screens/ONBOARDING/Otp.jsx
@@ -4,23 +4,61 @@ import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 // Components
 import Header from "../../Components/ONBOARDING/Header";
 
+// theme
+import { COLORS } from "../../constants/theme";
+
 // img
 import img from "./../../../assets/Login/otp.png";
 
 const Otp = () => {
   return (
-    <View>
+    <View style={OtpCss.mDIv}>
       <Header true={true} msg="Enter your Details" />
-      <Text>Otp</Text>
       <Image source={img} style={OtpCss.img} />
+      <Text style={OtpCss.Enter}>Enter the OTP</Text>
+      <View style={OtpCss.ViewNumber}>
+        <TextInput style={OtpCss.inpNumber} keyboardType="numeric" />
+        <TextInput style={OtpCss.inpNumber} keyboardType="numeric" />
+        <TextInput style={OtpCss.inpNumber} keyboardType="numeric" />
+        <TextInput style={OtpCss.inpNumber} keyboardType="numeric" />
+      </View>
+
+      <Text>Otp</Text>
     </View>
   );
 };
 
 const OtpCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Enter: {
+    marginBottom: 30,
+    color: COLORS.primary,
+    fontWeight: 700,
+    fontSize: 21,
+  },
   img: {
     width: "80%",
     height: "30%",
+    marginBottom: 20,
+  },
+  ViewNumber: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  inpNumber: {
+    padding: 12,
+    borderColor: "#6B4EFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    width: 50,
+    height: 50,
     marginBottom: 20,
   },
 });
