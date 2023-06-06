f46bed58f13bb7392199d16514bbd22c5d1c51db
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 17:06:58 +0530

    Update InpOne.jsx

--- src/Components/Cart/InpOne.jsx
+++ src/Components/Cart/InpOne.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
+// theme
+import { COLORS } from "../../constants/theme";
+
 const InpOne = () => {
   return (
     <View style={InpOneCss.imp1View}>
@@ -12,13 +15,22 @@ const InpOne = () => {
 
 const InpOneCss = StyleSheet.create({
   imp1View: {
+    marginTop: 20,
     width: "80%",
+    marginBottom: 20,
   },
   StoreName: {
+    color: "#626262",
     fontWeight: 600,
     fontSize: 20,
+    marginBottom: 10,
   },
-  inpNumber: {},
+  inpNumber: {
+    borderWidth: 1,
+    borderColor: COLORS.ActivityBlock,
+    borderRadius: 10,
+    padding: 12,
+  },
 });
 
 export default InpOne;
