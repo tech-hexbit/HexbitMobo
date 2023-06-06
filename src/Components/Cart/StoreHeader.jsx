94d75bf9b55e3b398c0c3ddfa52415fd47f66e40
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 23:52:59 +0530

    Update StoreHeader.jsx

--- src/Components/Cart/StoreHeader.jsx
+++ src/Components/Cart/StoreHeader.jsx
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
-const StoreHeader = () => {
+// theme
+import { COLORS } from "../../constants/theme";
+
+const StoreHeader = (props) => {
   return (
-    <View>
-      <Text>StoreHeader</Text>
+    <View style={StoreHeaderCss.mDiv}>
+      {props.true === true ? (
+        <Text style={StoreHeaderCss.Welcome}>Welcome to,</Text>
+      ) : (
+        ""
+      )}
+      <Text style={StoreHeaderCss.Hexbit}>Hexbit.io</Text>
     </View>
   );
 };
+
+const StoreHeaderCss = StyleSheet.create({
+  mDiv: {
+    width: "100%",
+    paddingLeft: 44,
+    marginBottom: 30,
+  },
+  Welcome: {
+    color: COLORS.primary,
+    fontSize: 31,
+    fontWeight: 400,
+  },
+  Hexbit: {
+    color: COLORS.primary,
+    fontSize: 31,
+    fontWeight: 900,
+  },
+});
 
 export default StoreHeader;
