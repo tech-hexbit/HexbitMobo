2d712f9771bde1e6fb155112f46aff17644558c8
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 16:54:47 +0530

    Update InpOne.jsx

--- src/Components/Cart/InpOne.jsx
+++ src/Components/Cart/InpOne.jsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 
 const InpOne = () => {
   return (
