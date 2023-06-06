d556fd5036c127a38f1b1ff8955415e6ca981568
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 18:31:26 +0530

    Update StoreFinal.jsx

--- src/Components/Cart/StoreFinal.jsx
+++ src/Components/Cart/StoreFinal.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+// img
+import img from "./../../../assets/Cart/StoreFinal.png";
 
 const StoreFinal = () => {
   return (
-    <View>
+    <View style={SFCss.mDiv}>
+      <Image source={img} />
       <Text>StoreFinal</Text>
     </View>
   );
 };
+
+const SFCss = StyleSheet.create({
+  mDiv: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#CCCEFF",
+  },
+});
 
 export default StoreFinal;
