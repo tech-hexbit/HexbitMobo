5b4134ee75588a16e5dd1a54af3b60877442b52d
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 16:53:34 +0530

    Create InpTwo.jsx

---
+++ src/Components/Cart/InpTwo.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const InpTwo = () => {
+  return (
+    <View>
+      <Text>InpTwo</Text>
+    </View>
+  );
+};
+
+export default InpTwo;
