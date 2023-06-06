14ecfdb484c781d3d244125dd5939d85aec26d2c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 09:07:26 +0530

    Update Header.jsx

--- src/Components/ONBOARDING/Header.jsx
+++ src/Components/ONBOARDING/Header.jsx
@@ -1,13 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+
+// theme
+import { COLORS } from "../../constants/theme";
 
 const Header = (props) => {
   return (
-    <View>
-      <Text>Welcome to,</Text>
-      <Text>Hexbit.io</Text>
+    <View style={HeaderCss.mDiv}>
+      {props.true === true ? (
+        <Text style={HeaderCss.Welcome}>Welcome to,</Text>
+      ) : (
+        ""
+      )}
+      <Text style={HeaderCss.Hexbit}>Hexbit.io</Text>
     </View>
   );
 };
+
+const HeaderCss = StyleSheet.create({
+  mDiv: {
+    width: "100%",
+    paddingLeft: 44,
+    marginBottom: 30,
+  },
+  Welcome: {
+    color: COLORS.primary,
+    fontSize: 31,
+    fontWeight: 400,
+  },
+  Hexbit: {
+    color: COLORS.primary,
+    fontSize: 31,
+    fontWeight: 900,
+  },
+});
 
 export default Header;
