a03d48953e15b8850d9d5884691718f31887ce7e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 08:57:50 +0530

    Update WhatsApp.jsx

--- src/screens/ONBOARDING/WhatsApp.jsx
+++ src/screens/ONBOARDING/WhatsApp.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 // Components
 import Header from "../../Components/ONBOARDING/Header";
 
+// theme
+import { COLORS } from "../../constants/theme";
+
+// img
+import img from "./../../../assets/Login/WhatsApp.png";
+
 const WhatsApp = () => {
   return (
-    <View>
+    <View style={WhatsAppCss.mDIv}>
       <Header true={true} msg="Enter your Details" />
+      <Image source={img} />
       <Text>WhatsApp</Text>
     </View>
   );
 };
+
+const WhatsAppCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: COLORS.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default WhatsApp;
