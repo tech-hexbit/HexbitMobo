6b84fa17d4da9fc96528414cf3ac4749df7c8694
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 00:18:33 +0530

    Update Store.jsx

--- src/Components/Cart/Store.jsx
+++ src/Components/Cart/Store.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 // components
@@ -9,10 +9,20 @@ const Store = () => {
   return (
     <View>
       <StoreHeader true={true} title="MyStore" />
+      <Text style={StoreCss.Products}>Products</Text>
       <ProductCart />
-      <Text>Store</Text>
     </View>
   );
 };
+
+const StoreCss = StyleSheet.create({
+  Products: {
+    color: "#350B5E",
+    fontWeight: 700,
+    fontSize: 20,
+    marginVertical: 10,
+    marginLeft: 20,
+  },
+});
 
 export default Store;
