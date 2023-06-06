84793c32ddfe2cdb40f9a7f41c54c80f93f57b4e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 17:47:16 +0530

    Update InpTwo.jsx

--- src/Components/Cart/InpTwo.jsx
+++ src/Components/Cart/InpTwo.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
+
+// Components
+import Header from "./Header";
+
+// img
+import img from "./../../../assets/Cart/cartGroceries.png";
 
 const InpTwo = () => {
   return (
-    <View>
-      <Text>InpTwo</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={InpTwoCss.mDiv}>
+        <Header Header="SETUP YOUR STORE" img={img} />
+
+        <Text style={InpTwoCss.t1}>Tell us more about your store</Text>
+        <Text style={InpTwoCss.t2}>
+          Add a short description about your store
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const InpTwoCss = StyleSheet.create({
+  mDiv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  t1: {
+    fontSize: 20,
+    fontWeight: 900,
+    color: "#350B5E",
+  },
+  t2: {
+    fontSize: 20,
+    fontWeight: 900,
+    color: "#350B5E",
+  },
+});
 
 export default InpTwo;
