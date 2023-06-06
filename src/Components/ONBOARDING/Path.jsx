2b3c00f9da2eef9a7a1c4c161bc0d104167f06f9
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 12:41:24 +0530

    Create Path.jsx

---
+++ src/Components/ONBOARDING/Path.jsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import React from "react";
+
+const Path = () => {
+  return (
+    <View>
+      <Text>Path</Text>
+    </View>
+  );
+};
+
+export default Path;
