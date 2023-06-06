e29660fd30cef48774e8330e9d1aaa4e89021f4e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 08:48:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // screens
-import Login from "./src/screens/Login";
+import Login from "./src/screens/ONBOARDING/Login";
+import Otp from "./src/screens/ONBOARDING/Otp";
 
 // Bottom Nav
 import BottomNav from "./src/Components/BottomNav";
@@ -16,7 +17,8 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="BottomNav" component={BottomNav} />
+          <Stack.Screen name="Otp" component={Otp} />
+          <Stack.Screen name="Home" component={BottomNav} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
