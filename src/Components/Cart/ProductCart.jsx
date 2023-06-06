d1a3706dbd9f6fa3596e117879a6fa3457540461
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 00:21:58 +0530

    Update ProductCart.jsx

--- src/Components/Cart/ProductCart.jsx
+++ src/Components/Cart/ProductCart.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const ProductCart = () => {
   return (
-    <View>
+    <View style={ProductCartCss.mDiv}>
       <Text>ProductCart</Text>
     </View>
   );
 };
+
+const ProductCartCss = StyleSheet.create({
+  mDiv: {
+    width: "90%",
+    backgroundColor: "red",
+    borderRadius: 10,
+  },
+});
 
 export default ProductCart;
