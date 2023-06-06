f0609f148e80623d252460052322ec54918caa85
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 17:01:36 +0530

    Update InpOne.jsx

--- src/Components/Cart/InpOne.jsx
+++ src/Components/Cart/InpOne.jsx
@@ -3,14 +3,21 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const InpOne = () => {
   return (
-    <View>
-      <Text>Store Name</Text>
+    <View style={InpOneCss.imp1View}>
+      <Text style={InpOneCss.StoreName}>Store Name</Text>
       <TextInput style={InpOneCss.inpNumber} placeholder="ABC Store" />
     </View>
   );
 };
 
 const InpOneCss = StyleSheet.create({
+  imp1View: {
+    width: "80%",
+  },
+  StoreName: {
+    fontWeight: 600,
+    fontSize: 20,
+  },
   inpNumber: {},
 });
 
