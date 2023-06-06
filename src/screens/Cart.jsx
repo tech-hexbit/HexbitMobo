f1f339fbb5b9afc53f3406fed3936aa81f83526e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 16:58:10 +0530

    Update Cart.jsx

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -1,7 +1,10 @@
 import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import React from "react";
 
+// Components
 import Header from "../Components/Cart/Header";
+import InpOne from "../Components/Cart/InpOne";
+import InpTwo from "../Components/Cart/InpTwo";
 
 // img
 import img from "./../../assets/Cart/cartGroceries.png";
@@ -12,6 +15,8 @@ const Cart = () => {
       <View style={CartCss.mDiv}>
         <Header Header="SETUP YOUR STORE" />
         <Image source={img} style={CartCss.img} />
+        <InpOne />
+        <InpTwo />
         <Text>Cart</Text>
       </View>
     </ScrollView>
