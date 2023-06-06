9afef592d08bec0a9f0c359bc14c84e41c01655b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 18:23:55 +0530

    Create StoreFinal.jsx

---
+++ src/Components/Cart/StoreFinal.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const StoreFinal = () => {
+  return (
+    <View>
+      <Text>StoreFinal</Text>
+    </View>
+  );
+};
+
+export default StoreFinal;
