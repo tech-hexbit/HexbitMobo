95b4e485fdd079e5f3fccbb37ff50ace34544ca1
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 12:38:59 +0530

    Update Name.jsx

--- src/screens/ONBOARDING/Name.jsx
+++ src/screens/ONBOARDING/Name.jsx
@@ -1,12 +1,78 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+
+// Components
+import Header from "../../Components/ONBOARDING/Header";
+
+// theme
+import { COLORS } from "../../constants/theme";
 
 const Name = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Name</Text>
+    <View style={NameCss.mDIv}>
+      <Header true={false} msg="Enter your Details" />
+      <Text style={NameCss.Enter}>Enter your Name</Text>
+
+      <LinearGradient
+        start={{ x: 0, y: 0.75 }}
+        end={{ x: 1, y: 0.25 }}
+        colors={["#BB14E2", "#161FE4"]}
+        style={NameCss.button}
+      >
+        <Text
+          style={NameCss.SendOTP}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          Next
+        </Text>
+      </LinearGradient>
     </View>
   );
 };
+
+const NameCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    width: "80%",
+    height: "30%",
+    marginBottom: 20,
+  },
+  Enter: {
+    marginBottom: 30,
+    color: COLORS.primary,
+    fontWeight: 700,
+    fontSize: 21,
+  },
+  inpNumber: {
+    padding: 12,
+    borderColor: "#6B4EFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    width: "80%",
+    marginBottom: 20,
+  },
+  button: {
+    borderRadius: 10,
+    paddingTop: 14,
+    paddingBottom: 14,
+    width: "70%",
+  },
+  SendOTP: {
+    color: "#fff",
+    fontWeight: 600,
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
 
 export default Name;
