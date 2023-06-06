10c8a85bdc8ed7586768886aa33e574e26a23144
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 00:10:13 +0530

    Create ProductCart.jsx

---
+++ src/Components/Cart/ProductCart.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProductCart = () => {
+  return (
+    <View>
+      <Text>ProductCart</Text>
+    </View>
+  );
+};
+
+export default ProductCart;
