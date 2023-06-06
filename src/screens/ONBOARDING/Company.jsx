893d8793f13527c3413deab8fea79f872fe051d9
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 13:19:53 +0530

    Create Company.jsx

---
+++ src/screens/ONBOARDING/Company.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Company = () => {
+  return (
+    <View>
+      <Text>Company</Text>
+    </View>
+  );
+};
+
+export default Company;
