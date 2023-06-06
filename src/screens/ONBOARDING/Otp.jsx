b763a9641bba06b4736ac2a59c36ad51281a713e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 10:38:20 +0530

    Update Otp.jsx

--- src/screens/ONBOARDING/Otp.jsx
+++ src/screens/ONBOARDING/Otp.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 
 // Components
 import Header from "../../Components/ONBOARDING/Header";
+
+// img
+import img from "./../../../assets/Login/otp.png";
 
 const Otp = () => {
   return (
     <View>
       <Header true={true} msg="Enter your Details" />
       <Text>Otp</Text>
+      <Image source={img} style={OtpCss.img} />
     </View>
   );
 };
+
+const OtpCss = StyleSheet.create({
+  img: {
+    width: "80%",
+    height: "30%",
+    marginBottom: 20,
+  },
+});
 
 export default Otp;
