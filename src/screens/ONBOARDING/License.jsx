18b85144090dba93ef49c4ac6d417a7149995a57
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 13:32:14 +0530

    Create License.jsx

---
+++ src/screens/ONBOARDING/License.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+
+// Components
+import Header from "../../Components/ONBOARDING/Header";
+import Path from "../../Components/ONBOARDING/Path";
+
+// theme
+import { COLORS } from "../../constants/theme";
+
+// img
+import img from "./../../../assets/Login/company.png";
+
+const License = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={LicenseCss.mDIv}>
+      <Header true={false} msg="Enter your Details" />
+      <Path img={img} pos={4} />
+      <Text>License</Text>
+    </View>
+  );
+};
+
+const LicenseCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    width: "80%",
+    height: "30%",
+    marginBottom: 20,
+  },
+  Enter: {
+    marginBottom: 30,
+    color: COLORS.primary,
+    fontWeight: 700,
+    fontSize: 21,
+  },
+  inpNumber: {
+    padding: 12,
+    borderColor: "#6B4EFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    width: "80%",
+    marginBottom: 20,
+  },
+  button: {
+    borderRadius: 10,
+    paddingTop: 14,
+    paddingBottom: 14,
+    width: "70%",
+  },
+  SendOTP: {
+    color: "#fff",
+    fontWeight: 600,
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
+
+export default License;
