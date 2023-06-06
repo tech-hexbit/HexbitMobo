4550891be66b4e172b7a4bffd752990c0bb3be96
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 12:25:08 +0530

    Create Name.jsx

---
+++ src/screens/ONBOARDING/Name.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Name = () => {
+  return (
+    <View>
+      <Text>Name</Text>
+    </View>
+  );
+};
+
+export default Name;
