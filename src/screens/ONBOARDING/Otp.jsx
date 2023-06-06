069320c1b18dddba45ef77c0ed0cac48b7ebe4df
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-06 08:51:40 +0530

    Update Otp.jsx

--- src/screens/ONBOARDING/Otp.jsx
+++ src/screens/ONBOARDING/Otp.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+// Components
+import Header from "../../Components/ONBOARDING/Header";
+
 const Otp = () => {
   return (
     <View>
+      <Header true={true} msg="Enter your Details" />
       <Text>Otp</Text>
     </View>
   );
