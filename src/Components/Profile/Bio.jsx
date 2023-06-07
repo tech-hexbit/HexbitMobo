630df29b2fb0bdb6a7d08ff783b2c2976a0a7af0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:35:00 +0530

    Update Bio.jsx

--- src/Components/Profile/Bio.jsx
+++ src/Components/Profile/Bio.jsx
@@ -7,7 +7,30 @@ import BioCss from "./Css/BioCss";
 const Bio = () => {
   return (
     <View style={BioCss.mDiv}>
-      <Text>Bio</Text>
+      <View>
+        <View>
+          <Text>Store Name</Text>
+          <Text>Store ABC</Text>
+        </View>
+        <View style={BioCss.SwitchStoreView}>
+          <Text style={BioCss.SwitchStore}>Edit Profile</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text>Name</Text>
+        <Text>Raghav Anand</Text>
+      </View>
+
+      <View>
+        <Text>Email</Text>
+        <Text>raghavanand@gmail.com</Text>
+      </View>
+
+      <View>
+        <Text>Nature of the company</Text>
+        <Text>Retail</Text>
+      </View>
     </View>
   );
 };
