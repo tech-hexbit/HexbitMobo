762e8ad27b590c5311a62985d53ebe1e2d290b17
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:43:40 +0530

    Update Bank.jsx

---
+++ src/Components/Profile/Bank.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Bank = () => {
+  return (
+    <View>
+      <Text>Bank</Text>
+    </View>
+  );
+};
+
+export default Bank;
