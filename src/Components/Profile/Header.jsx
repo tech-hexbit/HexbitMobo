d07e4fca5665645b9dd560902b3f6cf47be2c449
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:15:18 +0530

    Update Header.jsx

--- src/Components/Profile/Header.jsx
+++ src/Components/Profile/Header.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import HCss from "./Css/HeaderCss";
+
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={HCss.mDiv}>
+      <Text style={HCss.Title}>Profile</Text>
+      <Text style={HCss.SwitchStore}>Switch Store</Text>
     </View>
   );
 };
