d8f82076d416574884296ab3cbe830892ccf0d2f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:59:06 +0530

    Update AnalyticsCss.jsx

--- src/Components/Profile/Css/AnalyticsCss.jsx
+++ src/Components/Profile/Css/AnalyticsCss.jsx
@@ -3,6 +3,17 @@ import { StyleSheet } from "react-native";
 // theme
 import { COLORS } from "../../../constants/theme";
 
-const HCss = StyleSheet.create({});
+const ACss = StyleSheet.create({
+  mDiv: {
+    paddingHorizontal: 20,
+    marginBottom: 30,
+  },
+  Analytics: {
+    color: "#350b5e",
+    fontSize: 20,
+    fontWeight: 700,
+    marginBottom: 10,
+  },
+});
 
-export default HCss;
+export default ACss;
