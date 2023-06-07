aae2a47397aae0cad7b4db58e436abc2b4d0b404
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 19:17:01 +0530

    Update Header.jsx

--- src/Components/Profile/Header.jsx
+++ src/Components/Profile/Header.jsx
@@ -1,16 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 // style
 import HCss from "./Css/HeaderCss";
 
 const Header = () => {
+  const navigation = useNavigation();
   return (
     <View style={HCss.mDiv}>
       <Text style={HCss.Title}>Profile</Text>
       <View style={HCss.SwitchViewMain}>
         <View style={HCss.SwitchStoreView}>
-          <Text style={HCss.SwitchStore}>Switch Store</Text>
+          <Text
+            style={HCss.SwitchStore}
+            onPress={() => {
+              navigation.navigate("SwitchStore");
+            }}
+          >
+            Switch Store
+          </Text>
         </View>
       </View>
     </View>
