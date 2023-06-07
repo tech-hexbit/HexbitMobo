252363aba0217542391b3a1b01243f86fba94542
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 19:10:05 +0530

    Create SwitchStore.jsx

---
+++ src/Components/Profile/SwitchStore.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SwitchStore = () => {
+  return (
+    <View>
+      <Text>SwitchStore</Text>
+    </View>
+  );
+};
+
+export default SwitchStore;
