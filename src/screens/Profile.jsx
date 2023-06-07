4c6d5d7e1d4a87c7b9110d513f44c8ca962848c0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:15:20 +0530

    Update Profile.jsx

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// Components
+import Header from "./../Components/Profile/Header";
+
 const Profile = () => {
   return (
     <View>
+      <Header />
       <Text>Profile</Text>
     </View>
   );
