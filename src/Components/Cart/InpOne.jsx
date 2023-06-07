7d26f15ee2d14b69855cd9da385be518b032a579
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 11:46:09 +0530

    Update InpOne.jsx

--- src/Components/Cart/InpOne.jsx
+++ src/Components/Cart/InpOne.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import {
   View,
   Text,
@@ -6,10 +7,9 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 
-// theme
-import { COLORS } from "../../constants/theme";
+// style
+import InpOneCss from "./Css/InpOneCss";
 
 const InpOne = (props) => {
   const navigation = useNavigation();
@@ -43,42 +43,5 @@ const InpOne = (props) => {
     </View>
   );
 };
-
-const InpOneCss = StyleSheet.create({
-  imp1View: {
-    marginTop: 20,
-    width: "80%",
-    marginBottom: 20,
-  },
-  StoreName: {
-    color: "#626262",
-    fontWeight: 600,
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  inpNumber: {
-    borderWidth: 1,
-    borderColor: COLORS.ActivityBlock,
-    borderRadius: 10,
-    padding: 12,
-    marginBottom: 10,
-  },
-  btnView: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-  },
-  btnNext: {
-    paddingVertical: 11,
-    backgroundColor: "#622AC3",
-    width: 100,
-    borderRadius: 10,
-  },
-  Next: {
-    color: "#fff",
-    fontWeight: 800,
-    fontSize: 18,
-    textAlign: "center",
-  },
-});
 
 export default InpOne;
