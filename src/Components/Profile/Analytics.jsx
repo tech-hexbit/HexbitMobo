6294b6bdd10aa345c4c87f486cba2d3329bc0c0c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 19:05:25 +0530

    Update Analytics.jsx

--- src/Components/Profile/Analytics.jsx
+++ src/Components/Profile/Analytics.jsx
@@ -1,13 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 // style
 import ACss from "./Css/AnalyticsCss";
 
+// img
+import img from "./../../../assets/Profile/Analytics.png";
+
 const Analytics = () => {
   return (
     <View style={ACss.mDiv}>
       <Text style={ACss.Analytics}>Analytics</Text>
+      <Image source={img} style={ACss.img} />
     </View>
   );
 };
