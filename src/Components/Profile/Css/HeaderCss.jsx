721ffe58fc501071ed6280c67459fd986f779fba
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:15:17 +0530

    Update HeaderCss.jsx

--- src/Components/Profile/Css/HeaderCss.jsx
+++ src/Components/Profile/Css/HeaderCss.jsx
@@ -3,6 +3,15 @@ import { StyleSheet } from "react-native";
 // theme
 import { COLORS } from "../../../constants/theme";
 
-const HCss = StyleSheet.create({});
+const HCss = StyleSheet.create({
+  mDiv: {
+    backgroundColor: "#ccceff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+});
 
 export default HCss;
