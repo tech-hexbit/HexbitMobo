87333b7793761e1df92adaaf9997fd5e182c8d5f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 11:21:17 +0530

    Create Edit.jsx

---
+++ src/Components/Cart/Edit.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Edit = () => {
+  return (
+    <View>
+      <Text>Edit</Text>
+    </View>
+  );
+};
+
+export default Edit;
