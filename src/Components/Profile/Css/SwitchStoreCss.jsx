ec46dc568fe3db2354346944a192f566961fd6f7
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 19:19:08 +0530

    Create SwitchStoreCss.jsx

---
+++ src/Components/Profile/Css/SwitchStoreCss.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS } from "../../../constants/theme";
+
+const SSCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default SSCss;
