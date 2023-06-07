e1e087ec07d84810863c78c438dceea7609e0623
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:35:02 +0530

    Update BioCss.jsx

--- src/Components/Profile/Css/BioCss.jsx
+++ src/Components/Profile/Css/BioCss.jsx
@@ -3,6 +3,26 @@ import { StyleSheet } from "react-native";
 // theme
 import { COLORS } from "../../../constants/theme";
 
-const BioCss = StyleSheet.create({ mDiv: { paddingHorizontal: 20 } });
+const BioCss = StyleSheet.create({
+  mDiv: { paddingHorizontal: 20 },
+  SwitchStoreView: {
+    width: 110,
+    height: 35,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#350B5E",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  SwitchStore: {
+    textAlign: "center",
+    color: "#350B5E",
+    fontSize: 15,
+    fontWeight: 600,
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+});
 
 export default BioCss;
