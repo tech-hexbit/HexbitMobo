96f2b3c786d721cf3094f7b0e649d873124caea6
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:54:41 +0530

    Update Bank.jsx

--- src/Components/Profile/Bank.jsx
+++ src/Components/Profile/Bank.jsx
@@ -1,10 +1,27 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// style
+import BankCss from "./Css/BankCss";
+
 const Bank = () => {
   return (
-    <View>
-      <Text>Bank</Text>
+    <View style={BankCss.mDiv}>
+      <Text style={BankCss.BankDetails}>Bank Details</Text>
+
+      <View>
+        <View style={BankCss.dataViewBlock}>
+          <Text style={BankCss.title}>State Bank of India</Text>
+        </View>
+        <View style={BankCss.dataViewBlock}>
+          <Text style={BankCss.title}>Account Name: </Text>
+          <Text style={BankCss.data}>06401450000560</Text>
+        </View>
+        <View style={BankCss.dataViewBlock}>
+          <Text style={BankCss.title}>IFSC Code: </Text>
+          <Text style={BankCss.data}>SBIN0003951</Text>
+        </View>
+      </View>
     </View>
   );
 };
