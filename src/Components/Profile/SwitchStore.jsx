8cbeac85a0474addb8fad7b799a827ccc57f631a
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 19:28:39 +0530

    Update SwitchStore.jsx

--- src/Components/Profile/SwitchStore.jsx
+++ src/Components/Profile/SwitchStore.jsx
@@ -1,13 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
 // style
 import SSCss from "./Css/SwitchStoreCss";
 
+// img
+import img from "./../../../assets/Profile/shop.png";
+
 const SwitchStore = () => {
   return (
     <View style={SSCss.mDIv}>
       <Text style={SSCss.Selectastore}>Select a store</Text>
+
+      <View style={SSCss.storeBlock}>
+        <Image source={img} />
+        <View style={SSCss.rightView}>
+          <Text style={SSCss.StoreName}>Store Name</Text>
+          <Text style={SSCss.StoreType}>Store Type</Text>
+        </View>
+      </View>
     </View>
   );
 };
