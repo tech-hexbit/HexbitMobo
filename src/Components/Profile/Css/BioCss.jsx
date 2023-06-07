7b073d613059646170a9914697a4cba089c33c5e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:28:40 +0530

    Create BioCss.jsx

---
+++ src/Components/Profile/Css/BioCss.jsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS } from "../../../constants/theme";
+
+const BioCss = StyleSheet.create({ mDiv: { padding: 20 } });
+
+export default BioCss;
