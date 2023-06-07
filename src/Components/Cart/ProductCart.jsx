e9f46d535cc80a992908e58c4315dc8fae55b447
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 09:50:12 +0530

    Update ProductCart.jsx

--- src/Components/Cart/ProductCart.jsx
+++ src/Components/Cart/ProductCart.jsx
@@ -1,25 +1,42 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 
-const ProductCart = () => {
+// img
+import img from "./../../../assets/Cart/pod.png";
+
+const ProductCart = (props) => {
   return (
-    <View style={ProductCartCss.mDiv}>
+    <View style={ProductCartCss.insDiv}>
+      <View style={ProductCartCss.leftDiv}>
+        <Image source={img} />
+      </View>
+      <View style={ProductCartCss.rightDiv}>
+        <Text>Product Name</Text>
+        <Text>
+          Filler text is text that shares some characteristics of a real written
+          text, but is random.
+        </Text>
+      </View>
       <Text>ProductCart</Text>
     </View>
   );
 };
 
 const ProductCartCss = StyleSheet.create({
-  mDiv: {
+  insDiv: {
     width: "90%",
-    backgroundColor: "red",
     borderRadius: 10,
     padding: 15,
-
-    shadowColor: "red",
-    shadowOffset: { width: -2, height: 40 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+    flexDirection: "row",
+    // flexWrap: "wrap",
+    gap: 10,
+    borderWidth: 1,
+  },
+  leftDiv: {
+    width: "45%",
+  },
+  rightDiv: {
+    width: "45%",
   },
 });
 
