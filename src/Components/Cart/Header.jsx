33bad14a527e61fa4bd8417e6e58a01cbfee9830
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 11:44:15 +0530

    Update Header.jsx

--- src/Components/Cart/Header.jsx
+++ src/Components/Cart/Header.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import { View, Text, Image } from "react-native";
 
 // style
 import HCss from "./Css/HCss";
