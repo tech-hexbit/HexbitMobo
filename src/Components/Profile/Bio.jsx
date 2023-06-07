76745449f7dff6b35619489bb60557d297b6c412
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-07 18:25:19 +0530

    Create Bio.jsx

---
+++ src/Components/Profile/Bio.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+// style
+
+const Bio = () => {
+  return (
+    <View>
+      <Text>Bio</Text>
+    </View>
+  );
+};
+
+export default Bio;
