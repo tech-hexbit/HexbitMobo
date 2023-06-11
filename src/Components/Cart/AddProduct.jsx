7605e9474c2fddc982d5e82ff8bfd36b1bb96496
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 15:42:01 +0530

    Update AddProduct.jsx

--- src/Components/Cart/AddProduct.jsx
+++ src/Components/Cart/AddProduct.jsx
@@ -1,4 +1,11 @@
-import { View, Text, Image, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
 // style
@@ -9,49 +16,56 @@ import img from "./../../../assets/Cart/plus-circle.png";
 
 const AddProduct = () => {
   return (
-    <View>
+    <ScrollView showsVerticalScrollIndicator={false}>
       <View>
-        <Text style={ADCss.AddProductText}>Add Product</Text>
-      </View>
+        <View>
+          <Text style={ADCss.AddProductText}>Add Product</Text>
+        </View>
 
-      <View style={ADCss.AddImgView}>
-        <View style={ADCss.AddImgBorder}>
-          <View style={ADCss.AddImgGrey}>
-            <Image source={img} style={ADCss.plusCircle}></Image>
-            <Text style={ADCss.AddpodText}>Add Product Image</Text>
+        <View style={ADCss.AddImgView}>
+          <View style={ADCss.AddImgBorder}>
+            <View style={ADCss.AddImgGrey}>
+              <Image source={img} style={ADCss.plusCircle}></Image>
+              <Text style={ADCss.AddpodText}>Add Product Image</Text>
+            </View>
           </View>
         </View>
-      </View>
 
-      <View style={ADCss.paddView}>
-        <Text style={ADCss.label}>Product Name</Text>
-        <TextInput style={ADCss.TextInputTag} placeholder="ABC Store" />
-      </View>
-
-      <View style={ADCss.rowInp}>
-        <View style={ADCss.w45Div}>
-          <Text style={ADCss.label}>Product Price</Text>
-          <TextInput style={ADCss.TextInputTag} placeholder="₹ 100" />
-        </View>
-        <View style={ADCss.w45Div}>
+        <View style={ADCss.paddView}>
           <Text style={ADCss.label}>Product Name</Text>
-          <TextInput style={ADCss.TextInputTag} placeholder="100" />
+          <TextInput style={ADCss.TextInputTag} placeholder="ABC Store" />
+        </View>
+
+        <View style={ADCss.rowInp}>
+          <View style={ADCss.w45Div}>
+            <Text style={ADCss.label}>Product Price</Text>
+            <TextInput style={ADCss.TextInputTag} placeholder="₹ 100" />
+          </View>
+          <View style={ADCss.w45Div}>
+            <Text style={ADCss.label}>Qty In Stock</Text>
+            <TextInput style={ADCss.TextInputTag} placeholder="100" />
+          </View>
+        </View>
+
+        <View style={ADCss.paddView}>
+          <Text style={ADCss.label}>Product Type</Text>
+          <TextInput
+            style={ADCss.TextInputTag}
+            placeholder="Select Product type"
+          />
+        </View>
+
+        <View style={ADCss.paddView}>
+          <Text style={ADCss.label}>Product Description</Text>
+          <TextInput
+            style={ADCss.TextInputTag}
+            multiline={true}
+            numberOfLines={4}
+            placeholder="ABC Store Description ...."
+          />
         </View>
       </View>
-
-      <View style={ADCss.paddView}>
-        <Text style={ADCss.label}>Product Type</Text>
-        <TextInput
-          style={ADCss.TextInputTag}
-          placeholder="Select Product type"
-        />
-      </View>
-
-      <View style={ADCss.paddView}>
-        <Text style={ADCss.label}>Product Name</Text>
-        <TextInput style={ADCss.TextInputTag} placeholder="ABC Store" />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
