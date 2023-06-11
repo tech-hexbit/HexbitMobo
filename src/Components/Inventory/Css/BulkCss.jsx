e83105788fd63c4ada449649a31437879464391f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 01:10:23 +0530

    Update BulkCss.jsx

--- src/Components/Inventory/Css/BulkCss.jsx
+++ src/Components/Inventory/Css/BulkCss.jsx
@@ -4,9 +4,20 @@ import { StyleSheet } from "react-native";
 import { COLORS } from "../../../constants/theme";
 
 const BulkCss = StyleSheet.create({
+  imgVIew: {
+    marginVertical: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   img: {
     width: 300,
     height: 300,
+  },
+  UploadFile: {
+    textAlign: "center",
+    color: "#350B5E",
+    fontWeight: 700,
+    fontSize: 25,
   },
 });
 
