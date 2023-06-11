e7032d07a8bda3039fc279fc1e00cfc6886bb081
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 00:55:40 +0530

    Create Bulk.jsx

---
+++ src/Components/Inventory/Bulk.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Bulk = () => {
+  return (
+    <View>
+      <Text>Bulk</Text>
+    </View>
+  );
+};
+
+export default Bulk;
