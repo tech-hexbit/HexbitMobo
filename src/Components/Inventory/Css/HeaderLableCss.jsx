a4b0c74bc9796e10233b815f88863dbdc9951aa0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 16:15:29 +0530

    Create HeaderLableCss.jsx

---
+++ src/Components/Inventory/Css/HeaderLableCss.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS } from "../../../constants/theme";
+
+const HLCss = StyleSheet.create({
+  mDIv: {
+    backgroundColor: "red",
+  },
+});
+
+export default HLCss;
