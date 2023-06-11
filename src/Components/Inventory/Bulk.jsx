3fba0d8c00ce804542a3f07007b5adf067df91f7
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 01:07:32 +0530

    Update Bulk.jsx

--- src/Components/Inventory/Bulk.jsx
+++ src/Components/Inventory/Bulk.jsx
@@ -1,11 +1,27 @@
-import { View, Text } from "react-native";
 import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+// style
+import BulkCss from "../Inventory/Css/BulkCss";
+
+// img
+import img from "./../../../assets/Inventory/bulk.png";
 
 const Bulk = () => {
   return (
-    <View>
-      <Text>Bulk</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View>
+        <Image source={img} style={BulkCss.img}></Image>
+        <Text>Bulk s</Text>
+      </View>
+    </ScrollView>
   );
 };
 
