fe6b88ac9ec26d3a3364f1c58334422ba8cbfb5c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 00:21:24 +0530

    Update ProductPage.jsx

--- src/Components/Inventory/ProductPage.jsx
+++ src/Components/Inventory/ProductPage.jsx
@@ -1,5 +1,13 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
 // style
 import PPCss from "../Inventory/Css/ProductPageCss";
