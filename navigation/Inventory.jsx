63ca846fae023f9eb8fa221f94b4f80dabf8e254
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 00:55:34 +0530

    Update Inventory.jsx

--- navigation/Inventory.jsx
+++ navigation/Inventory.jsx
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // screens
 import Inventory from "../src/screens/Home";
 import ProductPage from "../src/Components/Inventory/ProductPage";
+import Bulk from "../src/Components/Inventory/Bulk";
 
 export default function CartNav() {
   const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ export default function CartNav() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="InventoryScreen" component={Inventory} />
         <Stack.Screen name="ProductPage" component={ProductPage} />
+        <Stack.Screen name="Bulk" component={Bulk} />
       </Stack.Navigator>
     </>
   );
