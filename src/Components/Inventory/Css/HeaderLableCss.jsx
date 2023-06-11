5658b1d8b6ed21e32abf11c073b450d185a1309d
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 16:20:45 +0530

    Update HeaderLableCss.jsx

--- src/Components/Inventory/Css/HeaderLableCss.jsx
+++ src/Components/Inventory/Css/HeaderLableCss.jsx
@@ -5,8 +5,22 @@ import { COLORS } from "../../../constants/theme";
 
 const HLCss = StyleSheet.create({
   mDIv: {
-    backgroundColor: "red",
+    backgroundColor: "#ccceff",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
   },
+  rowEleView: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  labText: {
+    color: "#350B5E",
+    fontSize: 14,
+    fontWeight: 600,
+  },
+  labVal: {},
 });
 
 export default HLCss;
