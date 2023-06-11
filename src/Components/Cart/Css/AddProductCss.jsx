8454f783ed75a5a321cfd338ea4e17ce7510f5ad
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 15:09:02 +0530

    Create AddProductCss.jsx

---
+++ src/Components/Cart/Css/AddProductCss.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS } from "../../../constants/theme";
+
+const CartCss = StyleSheet.create({
+  mDiv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CartCss;
