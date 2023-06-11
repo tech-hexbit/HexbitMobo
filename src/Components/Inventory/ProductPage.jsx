6ac38289df4580d83add58c52da2bbff81ac1a2f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 00:18:44 +0530

    Update ProductPage.jsx

--- src/Components/Inventory/ProductPage.jsx
+++ src/Components/Inventory/ProductPage.jsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// img
+import img from "./../../../assets/Cart/pod.png";
+
 const ProductPage = () => {
   return (
     <View>
