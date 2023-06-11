b695a1120f4a1c46b3c6dda5349f456c52261c6e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 00:24:40 +0530

    Update ProductPage.jsx

--- src/Components/Inventory/ProductPage.jsx
+++ src/Components/Inventory/ProductPage.jsx
@@ -16,10 +16,13 @@ import PPCss from "../Inventory/Css/ProductPageCss";
 import img from "./../../../assets/Cart/pod.png";
 
 const ProductPage = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>ProductPage</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={PPCss.headerDiv}>
+        <Text style={PPCss.headerText}>Product Details</Text>
+      </View>
+    </ScrollView>
   );
 };
 
