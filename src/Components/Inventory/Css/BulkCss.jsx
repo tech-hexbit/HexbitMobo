0f82c5352081af1b1286caab6cc61ae799083e21
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-12 01:07:35 +0530

    Update BulkCss.jsx

--- src/Components/Inventory/Css/BulkCss.jsx
+++ src/Components/Inventory/Css/BulkCss.jsx
@@ -3,6 +3,11 @@ import { StyleSheet } from "react-native";
 // theme
 import { COLORS } from "../../../constants/theme";
 
-const BulkCss = StyleSheet.create({});
+const BulkCss = StyleSheet.create({
+  img: {
+    width: 300,
+    height: 300,
+  },
+});
 
 export default BulkCss;
