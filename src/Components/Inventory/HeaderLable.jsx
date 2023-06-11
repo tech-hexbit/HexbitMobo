ea7efd1859cf004a335015736c000d8f74419785
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 16:15:31 +0530

    Create HeaderLable.jsx

---
+++ src/Components/Inventory/HeaderLable.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+// style
+import HLCss from "./Css/HeaderLableCss";
+
+const HeaderLable = () => {
+  return (
+    <View style={HLCss.mDIv}>
+      <Text>HeaderLable</Text>
+    </View>
+  );
+};
+
+export default HeaderLable;
