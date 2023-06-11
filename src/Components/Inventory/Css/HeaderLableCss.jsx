57e9d01d6e2970c7f8a7e0b67329e5d5f244e46c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 16:22:44 +0530

    Update HeaderLableCss.jsx

--- src/Components/Inventory/Css/HeaderLableCss.jsx
+++ src/Components/Inventory/Css/HeaderLableCss.jsx
@@ -17,10 +17,18 @@ const HLCss = StyleSheet.create({
   },
   labText: {
     color: "#350B5E",
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: 600,
   },
-  labVal: {},
+  labVal: {
+    backgroundColor: "#350B5E",
+    color: "#fff",
+    marginLeft: 5,
+    paddingVertical: 3,
+    paddingHorizontal: 9,
+    fontWeight: 600,
+    fontSize: 7.56,
+  },
 });
 
 export default HLCss;
