6e7e100b51d63b343304211a487de4a884607090
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 15:25:01 +0530

    Update Edit.jsx

--- src/Components/Cart/Edit.jsx
+++ src/Components/Cart/Edit.jsx
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+
+// style
+import ADCss from "./Css/AddProductCss";
+
+// img
+import img from "./../../../assets/Cart/plus-circle.png";
 
 const Edit = () => {
   return (
     <View>
-      <Text>Edit</Text>
+      <View>
+        <Text style={ADCss.AddProductText}>Add Product</Text>
+      </View>
+
+      <View style={ADCss.AddImgView}>
+        <View style={ADCss.AddImgBorder}>
+          <View style={ADCss.AddImgGrey}>
+            <Image source={img} style={ADCss.plusCircle}></Image>
+            <Text style={ADCss.AddpodText}>Add Product Image</Text>
+          </View>
+        </View>
+        <Text>hi</Text>
+      </View>
     </View>
   );
 };
