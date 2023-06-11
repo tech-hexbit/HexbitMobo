98b5131084ebf8442764beb690b93e5827d48789
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-11 16:48:11 +0530

    Update InventoryCss.jsx

--- src/screens/Css/InventoryCss.jsx
+++ src/screens/Css/InventoryCss.jsx
@@ -1,31 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const InvCss = StyleSheet.create({
-  Products: {
-    color: "#350B5E",
-    fontWeight: 700,
-    fontSize: 20,
-    marginVertical: 10,
-    marginLeft: 20,
-  },
   ProductsGrid: {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     gap: 10,
-    marginBottom: 10,
-  },
-  Addpod: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  AddProduct: {
-    color: "#fff",
-    backgroundColor: "#622AC3",
-    borderRadius: 10,
-    paddingHorizontal: 22,
-    paddingVertical: 12,
     marginBottom: 10,
   },
 });
