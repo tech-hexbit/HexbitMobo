9c4bc2d28b8f8d6872a9ce2b77fef6bdc1f594c4
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-13 20:38:27 +0530

    Update auth-context.js

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,14 +1,55 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = (props) => {
+  const [isLoading, setLoading] = useState(false);
+  const [userToken, setuserToken] = useState(null);
+
+  const login = () => {
+    setLoading(true);
+    setuserToken("hello");
+    AsyncStorage.setItem("token", "hello");
+    setLoading(false);
+  };
+
+  const logout = () => {
+    setLoading(true);
+    setuserToken(null);
+    AsyncStorage.removeItem("token");
+    setLoading(false);
+  };
+
+  const isLoadingFun = async () => {
+    try {
+      setLoading(true);
+      let userToken = await AsyncStorage.getItem("token");
+      setuserToken(userToken);
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    isLoadingFun();
+  }, []);
   //   const contextValue = useMemo(() => ({}), []);
 
   const [WhatsAppNumber, setWhatsAppNumber] = useState(0);
 
   return (
-    <AuthContext.Provider value={{ WhatsAppNumber, setWhatsAppNumber }}>
+    <AuthContext.Provider
+      value={{
+        WhatsAppNumber,
+        setWhatsAppNumber,
+        login,
+        logout,
+        isLoading,
+        userToken,
+      }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
