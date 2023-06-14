17193284ceed918d9a19661b2b55ca288783105e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-14 10:00:31 +0530

    Update Store.jsx

--- src/Components/Cart/Store.jsx
+++ src/Components/Cart/Store.jsx
@@ -1,5 +1,11 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 // components
@@ -9,10 +15,53 @@ import ProductCart from "./ProductCart";
 // style
 import StoreCss from "./Css/StoreCss";
 
+// state
+import AuthContext from "./../../../store/auth-context";
+
+// axios
+import axios from "axios";
+
 const Store = (props) => {
+  const { WhatsAppNumber, setstoreID } = useContext(AuthContext);
+  const [showError, setError] = useState("");
+
+  useEffect(() => {
+    getData;
+  }, []);
+
+  const getData = async () => {
+    try {
+      console.log("first-->");
+
+      let data = {
+        StoreID: setstoreID,
+      };
+      const res = await axios.post(
+        `http://192.168.1.40:8000/api/App/cart/getStoreItems`,
+        data
+      );
+      if (res.data.status === true) {
+        setError("");
+        console.log("res.data");
+        console.log(res.data);
+      } else {
+        console.log(res);
+        setError("Error");
+      }
+    } catch (error) {
+      console.log(error);
+      setError("Error: An Unexpected Error Happened");
+    }
+  };
+
   const navigation = useNavigation();
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
+      <TouchableOpacity onPress={getData}>
+        <View>
+          <Text>hello</Text>
+        </View>
+      </TouchableOpacity>
       <View>
         <StoreHeader true={true} title="MyStore" />
         <Text style={StoreCss.Products}>Products</Text>
