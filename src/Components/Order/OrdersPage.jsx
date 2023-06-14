d370493f64066fc77d14e41a51373b94749d7448
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-14 18:26:56 +0530

    Update OrdersPage.jsx

--- src/Components/Order/OrdersPage.jsx
+++ src/Components/Order/OrdersPage.jsx
@@ -1,10 +1,32 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const OrdersPage = (props) => {
-  console.log(props);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    console.log("first");
+  };
+
+  console.log(props.route.params._id);
   return (
     <View>
+      <View>
+        <View>
+          <Text>Order No.</Text>
+          <Text>{props.route.params._id}</Text>
+        </View>
+        <View>
+          <Text></Text>
+          <Text></Text>
+        </View>
+        <View>
+          <Text></Text>
+          <Text></Text>
+        </View>
+      </View>
       <Text>OrdersPage</Text>
     </View>
   );
