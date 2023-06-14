4dc294de5a79c33344712f7ae345c6be0240d391
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-14 19:05:44 +0530

    Update OrderPage.jsx

--- src/Components/Order/Css/OrderPage.jsx
+++ src/Components/Order/Css/OrderPage.jsx
@@ -1,5 +1,36 @@
 import { StyleSheet } from "react-native";
 
-const OPCss = StyleSheet.create({});
+const OPCss = StyleSheet.create({
+  mDiv: {
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+  },
+  HMDiv: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    gap: 10,
+  },
+  cnName: {
+    marginVertical: 15,
+  },
+  labelTEXT: {
+    fontSize: 16,
+    fontWeight: 500,
+  },
+  val1: {
+    width: "40%",
+    // backgroundColor: "red",
+  },
+  valText: {
+    fontSize: 16,
+    fontWeight: 600,
+  },
+  cenDiv: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
 
 export default OPCss;
