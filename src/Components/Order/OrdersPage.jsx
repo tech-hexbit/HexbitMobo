6f0ae4c675c580ebb863a1116504cd6280936534
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-14 18:18:01 +0530

    Create OrdersPage.jsx

---
+++ src/Components/Order/OrdersPage.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OrdersPage = (props) => {
+  console.log(props);
+  return (
+    <View>
+      <Text>OrdersPage</Text>
+    </View>
+  );
+};
+
+export default OrdersPage;
