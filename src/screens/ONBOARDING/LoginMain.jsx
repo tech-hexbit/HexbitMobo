1bb3e5518ae69ae167199b2b636b4e6beeddf2ca
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-16 00:50:32 +0530

    Update LoginMain.jsx

--- src/screens/ONBOARDING/LoginMain.jsx
+++ src/screens/ONBOARDING/LoginMain.jsx
@@ -1,13 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React from "react";
 
 // style
 import LMCss from "./Css/LMCss";
 
+// img
+import img1 from "./../../../assets/Login/logo.png";
+
 const LoginMain = () => {
   return (
     <View style={LMCss.mDIv}>
-      <Text>LoginMain</Text>
+      <Image source={img1} />
+
+      <View style={LMCss.MDiv}>
+        <Text>Login</Text>
+        <TextInput style={LMCss.LoginText1} />
+        <TextInput style={LMCss.LoginText2} />
+      </View>
     </View>
   );
 };
