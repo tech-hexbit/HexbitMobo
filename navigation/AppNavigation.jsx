75370133a81a2492896fc2265a87fb38d7d071d2
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-16 00:24:51 +0530

    Update AppNavigation.jsx

--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -22,50 +22,65 @@ import AuthContext from "./../store/auth-context";
 const AppNavigation = () => {
   const { isLoading, userToken } = useContext(AuthContext);
 
+  if (isLoading) {
+    return (
+      <View
+        style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
+      >
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  }
+
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="WhatsApp"
-          component={WhatsApp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Otp"
-          component={Otp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Name"
-          component={Name}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Email"
-          component={Email}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Company"
-          component={Company}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="License"
-          component={License}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{ headerShown: false }}
-          component={BottomNav}
-        />
+      <Stack.Navigator initialRouteName={userToken !== null ? "Home" : "Login"}>
+        {userToken !== null ? (
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={BottomNav}
+          />
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="WhatsApp"
+              component={WhatsApp}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Otp"
+              component={Otp}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Name"
+              component={Name}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Email"
+              component={Email}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Company"
+              component={Company}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="License"
+              component={License}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
