5556a887933a5b92567d919b9f6f9fb7bedfb9bd
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-15 08:51:05 +0530

    Update WhatsApp.jsx

--- src/screens/ONBOARDING/WhatsApp.jsx
+++ src/screens/ONBOARDING/WhatsApp.jsx
@@ -8,6 +8,7 @@ import {
   Image,
   TextInput,
   Alert,
+  ScrollView,
   TouchableOpacity,
 } from "react-native";
 
@@ -25,6 +26,8 @@ import { COLORS } from "../../constants/theme";
 
 // img
 import img from "./../../../assets/Login/WhatsApp.png";
+
+import WhatsAppCss from "./Css/WhatsAppCss";
 
 const WhatsApp = () => {
   const [textInputValue, setTextInputValue] = useState("");
@@ -103,51 +106,5 @@ const WhatsApp = () => {
     </View>
   );
 };
-
-const WhatsAppCss = StyleSheet.create({
-  mDIv: {
-    flexGrow: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  img: {
-    width: "80%",
-    height: "30%",
-    marginBottom: 20,
-  },
-  Enter: {
-    width: "100%",
-    paddingLeft: 44,
-    marginBottom: 30,
-    color: COLORS.primary,
-    fontWeight: 700,
-    fontSize: 21,
-  },
-  inpNumber: {
-    padding: 12,
-    borderColor: "#6B4EFF",
-    borderWidth: 2,
-    borderRadius: 10,
-    width: "80%",
-    marginBottom: 20,
-  },
-  button: {
-    borderRadius: 10,
-    paddingTop: 14,
-    paddingBottom: 14,
-    width: "70%",
-  },
-  SendOTP: {
-    color: "#fff",
-    fontWeight: 600,
-    fontSize: 20,
-    textAlign: "center",
-  },
-  errorMsg: {
-    color: "#800000",
-    marginBottom: 15,
-  },
-});
 
 export default WhatsApp;
