932a7e0b21735c93091fa795c5e70d96889fd9be
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-16 00:31:14 +0530

    Create LoginMain.jsx

---
+++ src/screens/ONBOARDING/LoginMain.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LoginMain = () => {
+  return (
+    <View>
+      <Text>LoginMain</Text>
+    </View>
+  );
+};
+
+export default LoginMain;
