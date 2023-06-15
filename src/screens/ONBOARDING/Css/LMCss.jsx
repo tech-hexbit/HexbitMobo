a1521ebe8f6a4d28aba43ab1117f14410d207f0c
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-16 00:39:34 +0530

    Create LMCss.jsx

---
+++ src/screens/ONBOARDING/Css/LMCss.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS } from "../../../constants/theme";
+
+const LMCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: COLORS.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default LMCss;
