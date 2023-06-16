8d3014465e4cc4443a9489538c3f030ba6ffcc49
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-16 16:50:30 +0530

    Update auth-context.js

--- store/auth-context.js
+++ store/auth-context.js
@@ -6,11 +6,20 @@ const AuthContext = React.createContext();
 export const AuthContextProvider = (props) => {
   const [isLoading, setLoading] = useState(false);
   const [userToken, setuserToken] = useState(null);
+  const [WhatsAppNumber, setWhatsAppNumber] = useState(0);
+  const [storeID, setstoreID] = useState("");
+  const [userInfo, setuserInfo] = useState(null);
 
-  const login = () => {
+  const login = (token, userInfo) => {
+    console.log("+-----------------------");
+    console.log(token);
+    console.log(userInfo);
+
     setLoading(true);
-    setuserToken("hello");
-    AsyncStorage.setItem("token", "hello");
+    setuserToken(token);
+    setuserInfo(userInfo);
+    AsyncStorage.setItem("userInfo", userInfo);
+    AsyncStorage.setItem("token", token);
     setLoading(false);
   };
 
@@ -36,9 +45,6 @@ export const AuthContextProvider = (props) => {
     isLoadingFun();
   }, []);
   //   const contextValue = useMemo(() => ({}), []);
-
-  const [WhatsAppNumber, setWhatsAppNumber] = useState(0);
-  const [storeID, setstoreID] = useState("");
 
   return (
     <AuthContext.Provider
