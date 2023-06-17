6d03ded3949fdd78346c2b4ea9597c570463ab74
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-17 17:56:13 +0530

    Update InpTwo.jsx

--- src/Components/Cart/InpTwo.jsx
+++ src/Components/Cart/InpTwo.jsx
@@ -30,7 +30,8 @@ const InpTwo = (props) => {
 
   const navigation = useNavigation();
 
-  const { userInfo, setstoreID, updateData } = useContext(AuthContext);
+  const { userInfo, setstoreID, updateData, setWhatsAppNumber } =
+    useContext(AuthContext);
 
   // useEffect(() => {
   //   console.log(props.route.params);
@@ -54,34 +55,30 @@ const InpTwo = (props) => {
         StoreDescription: shoeDes,
       };
 
-      // console.log("data============");
-      // console.log(data);
+      console.log("data============");
+      console.log(data);
 
       try {
-        // const res = await axios.post(
-        //   `http://192.168.1.40:8000/api/App/cart/AddStore`,
-        //   data
-        // );
+        const res = await axios.post(
+          `http://192.168.1.40:8000/api/App/cart/AddStore`,
+          data
+        );
 
-        // if (res.data.status === true) {
-        //   setError("");
-        //   console.log("res.data");
-        //   console.log(res.data);
+        if (res.data.status === true) {
+          setError("");
+          console.log("res.data");
+          console.log(res.data);
 
-        //   setstoreID(res.data.storeID);
+          setstoreID(res.data.storeID);
+          updateData(res.data.storeID);
 
-        //   navigation.navigate("StoreFinal");
+          navigation.navigate("Store");
 
-        // setWhatsAppNumber(data.WhatsAppNumber);
-
-        // navigate.navigate("Home", {
-        //   WhatsAppNumber: `${data.WhatsAppNumber}`,
-        // });
-        // } else {
-        //   console.log(res);
-        //   setError("Error");
-        // }
-        updateData(userInfo.WhatsAppNumber);
+          setWhatsAppNumber(data.WhatsAppNumber);
+        } else {
+          console.log(res);
+          setError("Error");
+        }
       } catch (error) {
         console.log(error);
         setError("Error: An Unexpected Error Happened");
