2ddbfce6ba8eef9dc5180b625d4f8850aa356d79
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-19 22:05:09 +0530

    Update Discount.jsx

--- src/screens/Discount.jsx
+++ src/screens/Discount.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// style
+import DisCss from "./Css/DiscountCss";
+
 const Discount = () => {
   return (
-    <View>
+    <View style={DisCss.mDiv}>
       <Text>Discount</Text>
     </View>
   );
