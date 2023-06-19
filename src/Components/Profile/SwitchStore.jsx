9f258ed72b0d07031d2c81533b46e2e0f81dba44
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-19 15:39:19 +0530

    Update SwitchStore.jsx

--- src/Components/Profile/SwitchStore.jsx
+++ src/Components/Profile/SwitchStore.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 // style
@@ -61,37 +61,71 @@ const SwitchStore = () => {
     }
   };
   return (
-    <View style={SSCss.mDIv}>
-      <Text style={SSCss.Selectastore}>Select a store</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={SSCss.mDIv}>
+        <Text style={SSCss.Selectastore}>Select a store</Text>
 
-      <View style={SSCss.storeBlock}>
-        <Image source={img} style={SSCss.img} />
-        <View style={SSCss.rightView}>
-          <Text style={SSCss.StoreName}>Store Name</Text>
-          <Text style={SSCss.StoreType}>Store Type</Text>
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.storeBlock}>
+          <Image source={img} style={SSCss.img} />
+          <View style={SSCss.rightView}>
+            <Text style={SSCss.StoreName}>Store Name</Text>
+            <Text style={SSCss.StoreType}>Store Type</Text>
+          </View>
+        </View>
+
+        <View style={SSCss.createView}>
+          <Image source={plus} style={SSCss.plus} />
+          <Text
+            style={SSCss.CreateStore}
+            onPress={() => {
+              navigation.navigate("CartMain");
+            }}
+          >
+            Create Store
+          </Text>
         </View>
       </View>
-
-      <View style={SSCss.storeBlock}>
-        <Image source={img} style={SSCss.img} />
-        <View style={SSCss.rightView}>
-          <Text style={SSCss.StoreName}>Store Name</Text>
-          <Text style={SSCss.StoreType}>Store Type</Text>
-        </View>
-      </View>
-
-      <View style={SSCss.createView}>
-        <Image source={plus} style={SSCss.plus} />
-        <Text
-          style={SSCss.CreateStore}
-          onPress={() => {
-            navigation.navigate("CartMain");
-          }}
-        >
-          Create Store
-        </Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
