017369fed5fc56dc5ac204c4b121172b98f96061
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-23 01:13:49 +0530

    Update SwitchStore.jsx

--- src/Components/Profile/SwitchStore.jsx
+++ src/Components/Profile/SwitchStore.jsx
@@ -43,7 +43,7 @@ const SwitchStore = () => {
 
     try {
       const res = await axios.post(
-        `http://192.168.1.40:8000/api/App/Profile/getStoreList`,
+        `http://192.168.43.29:8000/api/App/Profile/getStoreList`,
         data
       );
 
