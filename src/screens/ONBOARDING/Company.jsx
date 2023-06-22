599d5af63adf94ccd190560b494f6d0136a2d5c2
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-23 01:13:31 +0530

    Update Company.jsx

--- src/screens/ONBOARDING/Company.jsx
+++ src/screens/ONBOARDING/Company.jsx
@@ -47,7 +47,7 @@ const Company = (props) => {
 
       try {
         const res = await axios.post(
-          `http://192.168.1.40:8000/api/App/onborading/Company`,
+          `http://192.168.43.29:8000/api/App/onborading/Company`,
           data
         );
 
