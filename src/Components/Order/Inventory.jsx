4733594a72b7d0830b10041dc7cecd53ebb532e4
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-23 09:56:27 +0530

    Update Inventory.jsx

--- src/Components/Order/Inventory.jsx
+++ src/Components/Order/Inventory.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-// theme
-import { COLORS, SIZES } from "../../constants/theme";
-
 // axios
 import axios from "axios";
 
 // env
 import { REACT_NATIVE_BASE_URL } from "@env";
+
+// style
+import InventoryCss from "./Css/InventoryCss";
 
 const Inventory = () => {
   const [tableData, setTableData] = useState();
@@ -137,136 +137,5 @@ const Inventory = () => {
     </View>
   );
 };
-
-const InventoryCss = StyleSheet.create({
-  StatusText: {
-    color: COLORS.primary,
-    fontWeight: 900,
-    fontSize: 20,
-    marginBottom: 15,
-  },
-  tabView: {
-    backgroundColor: COLORS.ActivityBlock,
-    borderRadius: 10,
-    marginBottom: 15,
-  },
-  ViewRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingRight: 10,
-    paddingLeft: 10,
-  },
-  txt1: {
-    color: "white",
-    padding: 12,
-    fontWeight: 500,
-    fontSize: 16,
-    width: 230,
-    borderColor: "white",
-    borderRightWidth: 1,
-    borderBottomWidth: 1,
-    marginTop: 10,
-  },
-  txt2: {
-    color: "white",
-    padding: 12,
-    fontWeight: 500,
-    fontSize: 16,
-    width: 120,
-    borderColor: "white",
-    borderBottomWidth: 1,
-    marginTop: 10,
-  },
-  txt3: {
-    color: "white",
-    padding: 12,
-    fontWeight: 500,
-    fontSize: 16,
-    width: 230,
-    borderColor: "white",
-    borderRightWidth: 1,
-    marginBottom: 10,
-  },
-  txt4: {
-    color: "white",
-    padding: 12,
-    fontWeight: 500,
-    fontSize: 16,
-    borderColor: "white",
-    marginBottom: 10,
-  },
-  toggleBtn: {
-    marginTop: 5,
-    marginBottom: 20,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignContent: "center",
-    borderColor: COLORS.ActivityBlock,
-    borderWidth: 1,
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-  selected: {
-    backgroundColor: COLORS.ActivityBlock,
-    textAlign: "center",
-    fontWeight: 500,
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: "25%",
-    color: "white",
-  },
-
-  All: {
-    color: COLORS.primary,
-    textAlign: "center",
-    fontWeight: 500,
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: "25%",
-    borderRightWidth: 1,
-    borderRightColor: COLORS.ActivityBlock,
-  },
-  Shipped: {
-    color: "#216445",
-    textAlign: "center",
-    fontWeight: 500,
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: "25%",
-    borderRightWidth: 1,
-    borderRightColor: COLORS.ActivityBlock,
-  },
-  Cancelled: {
-    color: "#882830",
-    textAlign: "center",
-    fontWeight: 500,
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: "25%",
-    borderRightWidth: 1,
-    borderRightColor: COLORS.ActivityBlock,
-  },
-  On: {
-    color: "#8f9d4b",
-    textAlign: "center",
-    fontWeight: 500,
-    paddingTop: 10,
-    paddingBottom: 10,
-    width: "25%",
-  },
-  tableRow: {
-    flexDirection: "row",
-    borderBottomWidth: 1,
-    borderBottomColor: "gray",
-    paddingVertical: 10,
-  },
-  headerCell: {
-    fontWeight: "bold",
-    flex: 1,
-  },
-  tableCell: {
-    flex: 1,
-  },
-});
 
 export default Inventory;
