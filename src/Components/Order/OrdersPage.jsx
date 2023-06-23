3d424268cc0480dad11d4ff9aa5a6d4fc1892ad5
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-23 19:40:10 +0530

    Update OrdersPage.jsx

--- src/Components/Order/OrdersPage.jsx
+++ src/Components/Order/OrdersPage.jsx
@@ -36,7 +36,7 @@ const OrdersPage = (props) => {
     <ScrollView showsVerticalScrollIndicator={false}>
       {show.length ? (
         <>
-          {show.map((val, key) => {
+          {/* {show.map((val, key) => {
             return (
               <View key={key}>
                 <View style={OPCss.HeaderView}>
@@ -115,7 +115,10 @@ const OrdersPage = (props) => {
                 </View>
               </View>
             );
-          })}
+          })} */}
+          <View style={OPCss.DataNoView}>
+            <Text style={OPCss.NoData}>No Data</Text>
+          </View>
         </>
       ) : (
         <View>
