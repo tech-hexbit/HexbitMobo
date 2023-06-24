e59bbdce355c20754f7cb9d3c126b58e242c904b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 23:42:30 +0530

    Update ProductPage.jsx

--- src/Components/Inventory/ProductPage.jsx
+++ src/Components/Inventory/ProductPage.jsx
@@ -58,72 +58,78 @@ const ProductPage = (props) => {
       <View style={PPCss.headerDiv}>
         <Text style={PPCss.headerText}>Product Details</Text>
       </View>
-      <View style={PPCss.AddImgView}>
-        <Image source={img} style={PPCss.pimg}></Image>
-      </View>
+      {showData.length > 0 ? (
+        <>
+          <View style={PPCss.AddImgView}>
+            <Image source={img} style={PPCss.pimg}></Image>
+          </View>
 
-      <View style={PPCss.RowView}>
-        <View style={PPCss.RowViewChild}>
-          <View>
-            <Text style={PPCss.val1}>Product No.</Text>
-            <Text style={PPCss.val2}>#6745867</Text>
-          </View>
-          <View>
-            <Text style={PPCss.val1}>Product Name</Text>
-            <Text style={PPCss.val2}>Product Name1</Text>
-          </View>
-        </View>
-        <View style={PPCss.RowViewChild}>
-          <View>
-            <Text style={PPCss.val1}>Price</Text>
-            <Text style={PPCss.val2}>₹ 1000</Text>
-          </View>
-          <View>
-            <Text style={PPCss.val1}>Product Type</Text>
-            <Text style={PPCss.val2}>Product Type</Text>
-          </View>
-        </View>
+          <View style={PPCss.RowView}>
+            <View style={PPCss.RowViewChild}>
+              <View>
+                <Text style={PPCss.val1}>Product No.</Text>
+                <Text style={PPCss.val2}>#6745867</Text>
+              </View>
+              <View>
+                <Text style={PPCss.val1}>Product Name</Text>
+                <Text style={PPCss.val2}>Product Name1</Text>
+              </View>
+            </View>
+            <View style={PPCss.RowViewChild}>
+              <View>
+                <Text style={PPCss.val1}>Price</Text>
+                <Text style={PPCss.val2}>₹ 1000</Text>
+              </View>
+              <View>
+                <Text style={PPCss.val1}>Product Type</Text>
+                <Text style={PPCss.val2}>Product Type</Text>
+              </View>
+            </View>
 
-        <View style={PPCss.desView}>
-          <Text style={PPCss.val1}>Product Description</Text>
-          <Text style={PPCss.val2des}>
-            Filler text is text that shares some characteristics of a real
-            written text, but is random.
-          </Text>
-        </View>
+            <View style={PPCss.desView}>
+              <Text style={PPCss.val1}>Product Description</Text>
+              <Text style={PPCss.val2des}>
+                Filler text is text that shares some characteristics of a real
+                written text, but is random.
+              </Text>
+            </View>
 
-        <View style={PPCss.RowViewChild}>
-          <View>
-            <Text style={PPCss.val1}>Shipping Period</Text>
-            <Text style={PPCss.val2}>7 Days</Text>
-          </View>
-          <View>
-            <Text style={PPCss.val1}>Cancellable</Text>
-            <Text style={PPCss.val2}>Yes</Text>
-          </View>
-        </View>
+            <View style={PPCss.RowViewChild}>
+              <View>
+                <Text style={PPCss.val1}>Shipping Period</Text>
+                <Text style={PPCss.val2}>7 Days</Text>
+              </View>
+              <View>
+                <Text style={PPCss.val1}>Cancellable</Text>
+                <Text style={PPCss.val2}>Yes</Text>
+              </View>
+            </View>
 
-        <View style={PPCss.RowViewChild}>
-          <View>
-            <Text style={PPCss.val1}>Return Period</Text>
-            <Text style={PPCss.val2}>7 Days</Text>
+            <View style={PPCss.RowViewChild}>
+              <View>
+                <Text style={PPCss.val1}>Return Period</Text>
+                <Text style={PPCss.val2}>7 Days</Text>
+              </View>
+              <View>
+                <Text style={PPCss.val1}>Returnable</Text>
+                <Text style={PPCss.val2}>Yes</Text>
+              </View>
+            </View>
           </View>
-          <View>
-            <Text style={PPCss.val1}>Returnable</Text>
-            <Text style={PPCss.val2}>Yes</Text>
-          </View>
-        </View>
-      </View>
 
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("Edit");
-        }}
-      >
-        <View style={PPCss.EditVIew}>
-          <Text style={PPCss.Edit}>Edit</Text>
-        </View>
-      </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Edit");
+            }}
+          >
+            <View style={PPCss.EditVIew}>
+              <Text style={PPCss.Edit}>Edit</Text>
+            </View>
+          </TouchableOpacity>
+        </>
+      ) : (
+        ""
+      )}
     </ScrollView>
   );
 };
