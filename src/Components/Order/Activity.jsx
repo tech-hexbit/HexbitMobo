a3ff9d74c4c83f01b7521237dda7b260a1621798
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:07:47 +0530

    Update Activity.jsx

--- src/Components/Order/Activity.jsx
+++ src/Components/Order/Activity.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-// theme
-import { COLORS, SIZES } from "../../constants/theme";
+// style
+import ActivityCss from "./Css/ActivityCss";
 
 const Activity = (props) => {
   return (
@@ -12,28 +12,5 @@ const Activity = (props) => {
     </View>
   );
 };
-
-const ActivityCss = StyleSheet.create({
-  ActivityBlock: {
-    backgroundColor: COLORS.ActivityBlock,
-    borderRadius: 10,
-    paddingTop: 12,
-    paddingBottom: 12,
-    paddingLeft: 25,
-    paddingRight: 25,
-  },
-  amtText: {
-    color: "white",
-    fontWeight: 500,
-    fontSize: 30,
-    textAlign: "center",
-  },
-  statusText: {
-    color: "white",
-    fontWeight: 500,
-    fontSize: 20,
-    textAlign: "center",
-  },
-});
 
 export default Activity;
