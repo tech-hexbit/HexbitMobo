9146b4cb03d83e1596da71e764b2ef5202d84021
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:12:25 +0530

    Update Activity.jsx

--- src/Components/Order/Activity.jsx
+++ src/Components/Order/Activity.jsx
@@ -6,7 +6,15 @@ import ActivityCss from "./Css/ActivityCss";
 
 const Activity = (props) => {
   return (
-    <View style={ActivityCss.ActivityBlock}>
+    <View
+      style={[
+        ActivityCss.ActivityBlock,
+        props.num == 1 ? ActivityCss.num1 : "",
+        props.num == 2 ? ActivityCss.num2 : "",
+        props.num == 3 ? ActivityCss.num3 : "",
+        props.num == 4 ? ActivityCss.num4 : "",
+      ]}
+    >
       <Text style={ActivityCss.amtText}>{props.amt}</Text>
       <Text style={ActivityCss.statusText}>{props.status}</Text>
     </View>
