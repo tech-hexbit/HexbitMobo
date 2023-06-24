8034181fe08cbe292da407c4ca6e2e4667558c99
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 23:07:59 +0530

    Update ProductCart.jsx

--- src/Components/Cart/ProductCart.jsx
+++ src/Components/Cart/ProductCart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
