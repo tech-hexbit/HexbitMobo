d24a2502e565a3538858a4bec29881d36ec14f17
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:51:29 +0530

    Update Activity.jsx

--- src/Components/Order/Activity.jsx
+++ src/Components/Order/Activity.jsx
@@ -15,18 +15,8 @@ const Activity = (props) => {
         props.num == 4 ? ActivityCss.num4 : "",
       ]}
     >
-      <View
-      // style={[
-      //   ActivityCss.ActivityBlock,
-      //   props.num == 1 ? ActivityCss.num1 : "",
-      //   props.num == 2 ? ActivityCss.num2 : "",
-      //   props.num == 3 ? ActivityCss.num3 : "",
-      //   props.num == 4 ? ActivityCss.num4 : "",
-      // ]}
-      >
-        <Text style={ActivityCss.amtText}>{props.amt}</Text>
-        <Text style={ActivityCss.statusText}>{props.status}</Text>
-      </View>
+      <Text style={ActivityCss.amtText}>{props.amt}</Text>
+      <Text style={ActivityCss.statusText}>{props.status}</Text>
     </TouchableOpacity>
   );
 };
