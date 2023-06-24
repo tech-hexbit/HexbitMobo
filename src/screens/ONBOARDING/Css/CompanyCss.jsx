b82e5c0c8cbc65ca982ce927e85a60544d6754b0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 19:59:06 +0530

    Update CompanyCss.jsx

---
+++ src/screens/ONBOARDING/Css/CompanyCss.jsx
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS } from "../../../constants/theme";
+
+const CompanyCss = StyleSheet.create({
+  mDIv: {
+    flexGrow: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  img: {
+    width: "80%",
+    height: "30%",
+    marginBottom: 20,
+  },
+  Enter: {
+    marginBottom: 30,
+    color: COLORS.primary,
+    fontWeight: 700,
+    fontSize: 21,
+  },
+  inpNumber: {
+    padding: 12,
+    borderColor: "#6B4EFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    width: "80%",
+    marginBottom: 20,
+  },
+  button: {
+    borderRadius: 10,
+    paddingTop: 14,
+    paddingBottom: 14,
+    width: "70%",
+    marginBottom: 20,
+  },
+  SendOTP: {
+    color: "#fff",
+    fontWeight: 600,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  errorMsg: {
+    color: "#800000",
+    marginBottom: 15,
+  },
+});
+
+export default CompanyCss;
