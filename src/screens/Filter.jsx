ba4e8fa65b8ee3eea3b34790a1cabc5058aab3e0
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:23:09 +0530

    Update Filter.jsx

--- src/screens/Filter.jsx
+++ src/screens/Filter.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput } from "react-native";
 import React from "react";
 
+// style
 import FilterCss from "../Components/Order/Css/FilterCss";
+
+// img
+import img from "./../../assets/Cart/search.png";
 
 const Filter = () => {
   return (
     <View>
+      <View style={FilterCss.inpView}>
+        <Image source={img} />
+        <TextInput style={FilterCss.inp} placeholder="Search Order"></TextInput>
+      </View>
+
       <Text>Filter</Text>
     </View>
   );
