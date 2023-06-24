5c3b3d2d07e102b91670425d8122498117c2a9a3
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:17:47 +0530

    Create Filter.jsx

---
+++ src/screens/Filter.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+import FilterCss from "../Components/Order/Css/FilterCss";
+
+const Filter = () => {
+  return (
+    <View>
+      <Text>Filter</Text>
+    </View>
+  );
+};
+
+export default Filter;
