acf0c2aac934e1e01dee9d40b27adb293e0abbc6
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:23:07 +0530

    Update FilterCss.jsx

--- src/Components/Order/Css/FilterCss.jsx
+++ src/Components/Order/Css/FilterCss.jsx
@@ -3,6 +3,22 @@ import { StyleSheet } from "react-native";
 // theme
 import { COLORS, SIZES } from "../../../constants/theme";
 
-const FilterCss = StyleSheet.create({});
+const FilterCss = StyleSheet.create({
+  inpView: {
+    paddingLeft: 15,
+    marginVertical: 10,
+    overflow: "hidden",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  inp: {
+    marginVertical: 10,
+    marginLeft: 24,
+    width: "100%",
+    fontSize: 20,
+  },
+});
 
 export default FilterCss;
