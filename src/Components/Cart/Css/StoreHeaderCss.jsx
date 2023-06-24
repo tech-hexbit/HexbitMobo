973f83d4aedd4bdbeb7b14b9633eec1d34e54211
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 17:00:58 +0530

    Update StoreHeaderCss.jsx

---
+++ src/Components/Cart/Css/StoreHeaderCss.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const StoreHeaderCss = StyleSheet.create({
+  mDiv: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#ccceff",
+  },
+  Welcome: {
+    color: COLORS.primary,
+    fontSize: 20,
+    fontWeight: 900,
+  },
+  Hexbit: {
+    color: COLORS.primary,
+    fontSize: 33,
+    fontWeight: 900,
+  },
+  inpView: {
+    paddingLeft: 15,
+    marginTop: 20,
+    overflow: "hidden",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  inp: {
+    marginVertical: 10,
+    marginLeft: 24,
+    width: "100%",
+    fontSize: 20,
+  },
+});
+
+export default StoreHeaderCss;
