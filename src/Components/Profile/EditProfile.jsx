f8c28436dc01438ca717756e3cfa1cd929bdb39e
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 07:51:40 +0530

    Update EditProfile.jsx

--- src/Components/Profile/EditProfile.jsx
+++ src/Components/Profile/EditProfile.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+// style
+import EditProfileCss from "./Css/EditProfileCss";
+
 const EditProfile = () => {
   return (
-    <View>
+    <View style={EditProfileCss.mDIV}>
       <Text>EditProfile</Text>
     </View>
   );
