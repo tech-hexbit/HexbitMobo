dc9d9c4ba47a548760a7fdfd1dd10a5ab72747d2
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:15:49 +0530

    Update FilterCss.jsx

---
+++ src/Components/Order/Css/FilterCss.jsx
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS, SIZES } from "../../../constants/theme";
+
+const FilterCss = StyleSheet.create({});
+
+export default FilterCss;
