af01a7268d83f2257ff0186d065018eb25b22c9f
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:01:25 +0530

    Update SalesCss.jsx

---
+++ src/Components/Order/Css/SalesCss.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+// theme
+import { COLORS, SIZES } from "../../../constants/theme";
+
+const SalesCss = StyleSheet.create({
+  SalesText: {
+    color: COLORS.primary,
+    fontWeight: 900,
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  ActivityRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+});
+
+export default SalesCss;
