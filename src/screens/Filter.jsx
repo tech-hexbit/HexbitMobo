574a7e905d77bc5c82d2763070066e58ffc68693
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 18:27:32 +0530

    Update Filter.jsx

--- src/screens/Filter.jsx
+++ src/screens/Filter.jsx
@@ -6,6 +6,7 @@ import FilterCss from "../Components/Order/Css/FilterCss";
 
 // img
 import img from "./../../assets/Cart/search.png";
+import dn_arrow from "./../../assets/Customer/dn_Arrow.png";
 
 const Filter = () => {
   return (
@@ -13,6 +14,17 @@ const Filter = () => {
       <View style={FilterCss.inpView}>
         <Image source={img} />
         <TextInput style={FilterCss.inp} placeholder="Search Order"></TextInput>
+      </View>
+
+      <View>
+        <View>
+          <Text>STATUS</Text>
+          <View>
+            <Text>Shipped</Text>
+            <Image source={dn_arrow} />
+          </View>
+        </View>
+        <View></View>
       </View>
 
       <Text>Filter</Text>
