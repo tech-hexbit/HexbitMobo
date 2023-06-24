764eae940a409754ce576cc36a6fa5bcb29aabeb
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-24 19:59:08 +0530

    Update Company.jsx

--- src/screens/ONBOARDING/Company.jsx
+++ src/screens/ONBOARDING/Company.jsx
@@ -14,6 +14,9 @@ import DropDownPicker from "react-native-dropdown-picker";
 // axios
 import axios from "axios";
 
+// style
+import CompanyCss from "./Css/CompanyCss";
+
 // env
 import { REACT_NATIVE_BASE_URL } from "@env";
 
@@ -153,50 +156,5 @@ const Company = (props) => {
     </ScrollView>
   );
 };
-
-const CompanyCss = StyleSheet.create({
-  mDIv: {
-    flexGrow: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  img: {
-    width: "80%",
-    height: "30%",
-    marginBottom: 20,
-  },
-  Enter: {
-    marginBottom: 30,
-    color: COLORS.primary,
-    fontWeight: 700,
-    fontSize: 21,
-  },
-  inpNumber: {
-    padding: 12,
-    borderColor: "#6B4EFF",
-    borderWidth: 2,
-    borderRadius: 10,
-    width: "80%",
-    marginBottom: 20,
-  },
-  button: {
-    borderRadius: 10,
-    paddingTop: 14,
-    paddingBottom: 14,
-    width: "70%",
-    marginBottom: 20,
-  },
-  SendOTP: {
-    color: "#fff",
-    fontWeight: 600,
-    fontSize: 20,
-    textAlign: "center",
-  },
-  errorMsg: {
-    color: "#800000",
-    marginBottom: 15,
-  },
-});
 
 export default Company;
