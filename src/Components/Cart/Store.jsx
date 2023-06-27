4636db3528b657aa56987853ea33ae3ca807a84b
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-27 09:59:42 +0530

    Update Store.jsx

--- src/Components/Cart/Store.jsx
+++ src/Components/Cart/Store.jsx
@@ -14,6 +14,7 @@ import StoreHeader from "./StoreHeader";
 import ProductCart from "./ProductCart";
 
 // style
+import InvCss from "./../../screens/Css/InventoryCss";
 import StoreCss from "./Css/StoreCss";
 
 // state
@@ -24,12 +25,14 @@ import axios from "axios";
 
 // img
 import refresh from "./../../../assets/refresh.png";
+import plus from "./../../../assets/Inventory/plus.png";
 
 const Store = (props) => {
   const { AddStore } = useContext(AuthContext);
 
   const [showData, setData] = useState([]);
   const [showError, setError] = useState("");
+  const [showOp, setOp] = useState(false);
 
   useEffect(() => {
     getData;
@@ -96,7 +99,43 @@ const Store = (props) => {
             </View>
           )}
         </View>
-        <View style={StoreCss.Addpod}>
+
+        <View style={InvCss.posrel}>
+          <View style={InvCss.posAbsMain}>
+            <View style={showOp ? InvCss.listshow : InvCss.list}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("AddProduct");
+                }}
+              >
+                <Text style={InvCss.opions}>Add a Product</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("Bulk");
+                }}
+              >
+                <Text style={InvCss.opions}>Import Bulk Products</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={InvCss.opions}>Export Products</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                setOp(!showOp);
+              }}
+            >
+              <View style={InvCss.posAbs}>
+                <Image source={plus} style={InvCss.plus} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* <View style={StoreCss.Addpod}>
           <Text
             style={StoreCss.AddProduct}
             onPress={() => {
@@ -105,7 +144,7 @@ const Store = (props) => {
           >
             Add Product
           </Text>
-        </View>
+        </View> */}
       </View>
     </ScrollView>
   );
