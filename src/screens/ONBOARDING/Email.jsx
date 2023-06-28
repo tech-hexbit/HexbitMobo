44bd9295adf791b865b67c55224d6f116c920805
Rishav Singh <rishavrajcrj@gmail.com>
2023-06-28 14:55:20 +0530

    Update Email.jsx

--- src/screens/ONBOARDING/Email.jsx
+++ src/screens/ONBOARDING/Email.jsx
@@ -8,6 +8,7 @@ import {
   Image,
   TextInput,
   ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 
 // axios
@@ -25,6 +26,9 @@ import { COLORS } from "../../constants/theme";
 
 // img
 import img from "./../../../assets/Login/email.png";
+
+// style
+import EmailCss from "./Css/EmailCss";
 
 const Email = (props) => {
   const [showError, setError] = useState("");
@@ -101,49 +105,5 @@ const Email = (props) => {
     </View>
   );
 };
-
-const EmailCss = StyleSheet.create({
-  mDIv: {
-    flexGrow: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  img: {
-    width: "80%",
-    height: "30%",
-    marginBottom: 20,
-  },
-  Enter: {
-    marginBottom: 30,
-    color: COLORS.primary,
-    fontWeight: 700,
-    fontSize: 21,
-  },
-  inpNumber: {
-    padding: 12,
-    borderColor: "#6B4EFF",
-    borderWidth: 2,
-    borderRadius: 10,
-    width: "80%",
-    marginBottom: 20,
-  },
-  button: {
-    borderRadius: 10,
-    paddingTop: 14,
-    paddingBottom: 14,
-    width: "70%",
-  },
-  SendOTP: {
-    color: "#fff",
-    fontWeight: 600,
-    fontSize: 20,
-    textAlign: "center",
-  },
-  errorMsg: {
-    color: "#800000",
-    marginBottom: 15,
-  },
-});
 
 export default Email;
